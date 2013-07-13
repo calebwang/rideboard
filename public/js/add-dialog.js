c31a81c50d9e9affbd9ae21039a77717b483f225
Caleb Wang <cw@berkeley.edu>
2013-07-13 00:06:40 -0700

    added modal add dialog

---
+++ public/js/add-dialog.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#add-passenger').click(function() {
+        $('#dialogModal').modal('show');
+    });
+    $('#dialog-close').click(function() {
+        $('#dialogModal').modal('hide');
+    });
+    $('#dialog-save').click(function() {
+        $('#dialogModal').modal('hide');
+    });
+});
