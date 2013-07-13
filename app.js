9293b1e628d7687ee592d5aac7400fb5a3c962f0
Caleb Wang <cw@berkeley.edu>
2013-07-13 03:52:27 -0700

    log data so we can see it

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.get('/:eventId', function(req, res) {
   console.log("get /eventId: " + req.params);
   var id = new ObjectId(req.params.eventId);
   db.events.find({ "_id": id}, function(err, data){
+    console.log(data[0]);
     console.log("get /eventId :" + data);
     res.render('dashboard', {eventId: id, event_data: data});
   });
